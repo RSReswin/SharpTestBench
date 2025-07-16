8e1e1890bc9c4a3169581fa5550f979dba400daa
RedEye Developers <75036674+RSReswin@users.noreply.github.com>
2025-07-16 17:58:57 +0530

    Enumerable_Bench and Eunmerator_Bench added

---
+++ SharpTestBench/Benches/Enumerable_Bench.cs
@@ -0,0 +1,33 @@
+﻿namespace SharpTestBench.Benches;
+
+public static class Enumerable_Bench
+{
+    public static void EnumerableWithCollection()
+    {
+       IEnumerable<int> items = [1,2,3,4,5];
+
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static void EnumerableWithMethodCollection()
+    {
+        var items = Numbers();
+
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    private static IEnumerable<int> Numbers()
+    {
+        yield return 1;
+        yield return 2;
+        yield return 3;
+        yield return 4;
+        yield return 5;
+    }
+}
---
+++ SharpTestBench/Benches/Enumerator_Bench.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace SharpTestBench.Benches;
+
+public static class Enumerator_Bench
+{
+    public static void IterateFromCollection()
+    {
+        var list = new List<int>(){1,2,3,4,5};
+        var items = list.GetEnumerator();
+
+        while (items.MoveNext())
+        {
+            Console.WriteLine(items.Current);
+        }
+        items.Dispose();
+    }
+}
